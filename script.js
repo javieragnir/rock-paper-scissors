5ccd17924f53f4282ddca95cbcaaac0b44fe1795
javier <jjr.agnir@gmail.com>
2022-06-21 16:50:52 +0800

    Add ability to play one game consisting of multiple rounds

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 // Main Function
-
-let playerSelection = prompt("Please enter your hand", "Rock, Paper, or Scissors");
-let computerSelection = computerPlay();
-
-console.log(playerSelection);
-console.log(computerSelection);
-
-playRound(playerSelection, computerSelection);
+game(5);
 
 // Helper Functions
 
@@ -21,8 +14,15 @@ function computerPlay() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
+    // Get play inputs from user and computer
+    let playerSelection = prompt("Please enter your hand", "Rock, Paper, or Scissors");
+    let computerSelection = computerPlay(); 
+
+    // Format user input
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+    // Determine result of round
     let key;
     if (playerSelection === computerSelection) {
         key = 0;
@@ -34,13 +34,37 @@ function playRound(playerSelection, computerSelection) {
         key = 2;
     }
 
+    // Print results of round
     if (key === 0) {
-        console.log(`You tied! You both selected ${playerSelection}.`)
+        console.log(`You tied! You both selected ${playerSelection}.`);
     } else if (key === 1) {
-        console.log(`You won! ${playerSelection} beats ${computerSelection}.`)
+        console.log(`You won! ${playerSelection} beats ${computerSelection}.`);
     } else {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}.`)
     }
+    // Return result of round
+    return key;
+}
 
-    return;
+function game(rounds) {
+    playerWins = 0;
+    computerWins = 0;
+
+    // Simulate rounds
+    for (let i = 0; i < rounds; i++) {
+        result = playRound();
+        if (result === 1) {
+            playerWins++;
+        } else if (result === 2) {
+            computerWins++;
+        }
+    }
+    // Print winner
+    if (playerWins > computerWins) {
+        console.log(`You win! You: ${playerWins}, Computer: ${computerWins}.`);
+    } else if (playerWins < computerWins) {
+        console.log(`You lose! You: ${playerWins}, Computer: ${computerWins}.`);
+    } else {
+        console.log(`Tie! You: ${playerWins}, Computer: ${computerWins}.`);
+    }
 }
