306e78ef5656bf0a24a4c6044cc4a582a38809e3
javier <jjr.agnir@gmail.com>
2022-06-21 16:39:37 +0800

    Add playRound function

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 // Main Function
 
-let playerSelection = prompt("Please enter your hand", "rock, paper, or scissors");
+let playerSelection = prompt("Please enter your hand", "Rock, Paper, or Scissors");
 let computerSelection = computerPlay();
 
 console.log(playerSelection);
 console.log(computerSelection);
+
+playRound(playerSelection, computerSelection);
 
 // Helper Functions
 
 function computerPlay() {
     number = Math.floor(Math.random() * 3);
     if (number == 0) {
-        return 'rock';
+        return 'Rock';
     } else if (number == 1) {
-        return 'paper';
+        return 'Paper';
     } else {
-        return 'scissors';
+        return 'Scissors';
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toUpperCase()
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let key;
+    if (playerSelection === computerSelection) {
+        key = 0;
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors'
+    || playerSelection === 'Paper' && computerSelection === 'Rock' 
+    || playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        key = 1;
+    } else {
+        key = 2;
+    }
+
+    if (key === 0) {
+        console.log(`You tied! You both selected ${playerSelection}.`)
+    } else if (key === 1) {
+        console.log(`You won! ${playerSelection} beats ${computerSelection}.`)
+    } else {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}.`)
+    }
+
     return;
 }
